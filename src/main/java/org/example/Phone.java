6308fab73ac37c7410c41798b3eb322a7114522d
Zakaria Salogo <zakariasalgo@yahoo.fr>
2022-04-27 10:56:00 -0500

    injectting the properties

---
+++ src/main/java/org/example/Phone.java
@@ -0,0 +1,24 @@
+package org.example;
+
+public class Phone {
+    private String mob;
+
+    public Phone() {
+    }
+
+    public Phone(String mob) {
+
+        this.mob = mob;
+    }
+
+    public void setMob(String mob) {
+        this.mob = mob;
+    }
+
+    @Override
+    public String toString() {
+        return "Phone{" +
+                "mob='" + mob + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/org/example/Student.java
@@ -0,0 +1,33 @@
+package org.example;
+
+import java.util.List;
+
+public class Student<Phone> {
+    private int id;
+    private String name;
+    private List<Phone> ph;
+    private Address add;
+
+    public Student(int id, String name, List<Phone> ph, Address add) {
+        this.id = id;
+        this.name = name;
+        this.ph = ph;
+        this.add = add;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", ph=" + ph +
+                ", add=" + add +
+                '}';
+    }
+
+    public void student(){
+
+        System.out.println("I'm a Genspark trainee..." + add );
+
+    }
+}
